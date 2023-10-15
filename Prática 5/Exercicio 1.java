1c758521fdb02b07b82063ddd2036efc5d8f981c
beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>
2023-10-15 18:48:33 -0300

    Create Exercicio 1.java

---
+++ Prática 5/Exercicio 1.java
@@ -0,0 +1,17 @@
+package lista.java;
+
+public class GitHubLista {
+
+	public static void main(String[] args) {
+		int inicio = 7;
+        int fim = 20;
+        int soma = 0;
+
+        for (int i = inicio; i <= fim; i++) {
+            soma += i;
+        }
+
+        System.out.println("A soma dos números entre " + inicio + " e " + fim + " é: " + soma);
+    }
+
+	}
