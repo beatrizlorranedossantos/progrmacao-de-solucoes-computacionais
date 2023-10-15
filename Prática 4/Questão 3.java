f9a5a3a6737ec546dc7435123f54b7a35ed25641
beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>
2023-10-15 18:41:15 -0300

    Create Questão 3.java

---
+++ Prática 4/Questão 3.java
@@ -0,0 +1,23 @@
+package exercicio4;
+
+import java.util.Scanner;
+
+public class Atividade3 {
+	 public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        String resposta;
+	 
+	         while (true) {  
+	        }
+	            // Coloque o código da prática aqui
+	       
+	        	System.out.println("Execução");
+
+	            // Pergunte ao usuário se deseja continuar
+	            System.out.print("Deseja continuar (S para sim, qualquer outra tecla para não)? ");
+	            resposta = scanner.nextLine();
+	            if (!resposta.equalsIgnoreCase("S")) {
+	            }break; // Saia do loop se a resposta não for "S"
+           }
+       }
+    }
