59e76b75830fc08826b2ec6ac735f7d51cc097e9
beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>
2023-10-15 21:57:42 -0300

    Update Atividade 7.java

--- Prática 5/Atividade 7.java
+++ Prática 5/Atividade 7.java
@@ -1,10 +1,27 @@
-package lista.java;
+package atividade5;
+import java.util.Scanner;
 
-public class ListaJavaAt7 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class exercicio7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int maior = Integer.MIN_VALUE; 
+        int menor = Integer.MAX_VALUE; 
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Digite o número " + i + ": ");
+            int numero = sc.nextInt();
+            
+            if (numero > maior) {
+                maior = numero;
+            }
+            
+            if (numero < menor) {
+                menor = numero;
+            }
+        }
+        
+        System.out.println("O maior número informado é: " + maior);
+        System.out.println("O menor número informado é: " + menor);
+    }
 }
