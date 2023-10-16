61d152861eb0c778ca10aeb421067ac63d673baf
beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>
2023-10-15 21:56:26 -0300

    Update Exercício 4.java

--- Prática 5/Exercício 4.java
+++ Prática 5/Exercício 4.java
@@ -1,10 +1,34 @@
-package lista.java;
-
-public class ListaJavaAt4 {
-
+package atividade5;
+import java.util.Scanner;
+public class exercicio4 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        Scanner scanner = new Scanner(System.in);
+        double media = 0;
+       int somaidades= 0;
+       int contador= 0;
+       while(true) {
+    	   System.out.println("Digite uma quantidade de numeros para calcular a media, caso queira interromper digite um numero negativo");
+    	   int idade = scanner.nextInt();
+    	   
+          somaidades+= idade;
+          contador++;
+        
+        
+          
+          
+          if(idade==-1) {
+        	 System.out.println("O numero tem que ser maior que 0");
+        	 break;
+          }
+        		
+    	   
+          media =  somaidades / contador;
 
+        
+        		
+       }
+    	   
+       System.out.println("Média das idades: " + media);
+       
 	}
-
 }
