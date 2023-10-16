4030456b54781ca44b28e8bcc2edc7341696fb75
beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>
2023-10-15 21:56:50 -0300

    Update Exercício 5.java

--- Prática 5/Exercício 5.java
+++ Prática 5/Exercício 5.java
@@ -1,10 +1,31 @@
-package lista.java;
+package atividade5;
+import java.util.Scanner;
 
-public class ListaJavaAt5 {
+public class exercicio5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int maior = Integer.MIN_VALUE; 
+        int menor = Integer.MAX_VALUE; 
 
-	}
+        while (true) {
+            System.out.println("Digite um número, caso queira interromper digite um número negativo:");
+            int n = input.nextInt();
 
+            if (n < 0) {
+                break; 
+            }
+
+            if (n > maior) {
+                maior = n;
+            }
+
+            if (n < menor) {
+                menor = n;
+            }
+        }
+
+        System.out.println("O maior número informado é: " + maior);
+        System.out.println("O menor número informado é: " + menor);
+    }
 }
