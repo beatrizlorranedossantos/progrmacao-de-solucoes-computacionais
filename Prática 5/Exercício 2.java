b8d92c8e5ca7cf47bc72269e28821968f48b49dc
beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>
2023-10-15 21:55:05 -0300

    Update Exercício 2.java

--- Prática 5/Exercício 2.java
+++ Prática 5/Exercício 2.java
@@ -1,10 +1,29 @@
-package lista.java;
+package atividade5;
+import java.util.Scanner;
 
-public class ListaJavaAtividade2 {
+public class exercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.print("Digite o primeiro número inteiro positivo: ");
+        int primeiroNumero = scanner.nextInt();
 
-	}
+        System.out.print("Digite o segundo número inteiro positivo: ");
+        int segundoNumero = scanner.nextInt();
 
+        if (primeiroNumero <= 0 || segundoNumero <= 0) {
+            System.out.println("Por favor, insira números inteiros positivos.");
+        } else {
+            int limiteInferior = Math.min(primeiroNumero, segundoNumero);
+            int limiteSuperior = Math.max(primeiroNumero, segundoNumero);
+
+            System.out.println("Números inteiros entre " + limiteInferior + " e " + limiteSuperior + ":");
+
+            for (int i = limiteInferior; i <= limiteSuperior; i++) {
+                System.out.print(i + " ");
+            }
+        }
+
+        scanner.close();
+    }
 }
