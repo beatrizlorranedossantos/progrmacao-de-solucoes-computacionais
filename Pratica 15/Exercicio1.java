f4e5b92204641cb1f2a63f263e427869b6bde995
beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>
2023-12-17 20:31:13 -0300

    Update Exercicio1.java

--- Pratica 15/Exercicio1.java
+++ Pratica 15/Exercicio1.java
@@ -1,1 +1,39 @@
+import java.util.Scanner;
+
+public class DivisaoInteiros {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Digite o dividendo (número a ser dividido): ");
+            int dividendo = scanner.nextInt();
+
+            System.out.print("Digite o divisor (número pelo qual dividir): ");
+            int divisor = scanner.nextInt();
+
+            int resultado = divide(dividendo, divisor);
+            System.out.println("Resultado da divisão: " + resultado);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Erro: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Erro inesperado: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+
+    public static int divide(int dividendo, int divisor) {
+        if (divisor == 0) {
+            throw new ArithmeticException("Divisão por zero não é permitida.");
+        }
+
+        if (dividendo < 0) {
+            throw new ArithmeticException("Dividendo não pode ser menor que 0.");
+        }
+
+        return dividendo / divisor;
+    }
+}
 
