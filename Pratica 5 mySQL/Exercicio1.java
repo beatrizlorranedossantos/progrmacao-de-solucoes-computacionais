b23d8915788bfe70d6e2c07144a6d69f155c1d6b
beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>
2023-12-18 08:25:20 -0300

    Update Exercicio1.sql

--- Pratica 5 mySQL/Exercicio1.java
+++ Pratica 5 mySQL/Exercicio1.java
@@ -1,1 +1,11 @@
+SELECT p.nome AS 'Produto', e.quantidade AS 'Quantidade', p.preco * e.quantidade AS 'Valor Total'
+FROM produto AS p
+JOIN estoque AS e ON p.id = e.id_produto
+ORDER BY 'Valor Total' DESC;
 
+SELECT f.nome AS 'Filial', SUM(p.preco * e.quantidade) AS 'Valor Total por Filial'
+FROM filial AS f
+JOIN estoque AS e ON f.cnpj = e.cnpj_filial
+JOIN produto AS p ON e.id_produto = p.id
+GROUP BY f.nome
+ORDER BY 'Valor Total por Filial' DESC;
