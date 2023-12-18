f797644719ab6178eaf34ac556a197b0a14801df
beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>
2023-12-18 08:23:01 -0300

    Update Exercicio2.sql

--- Pratica 3 mySQL/Exercicio2.sql
+++ Pratica 3 mySQL/Exercicio2.sql
@@ -1,26 +1,36 @@
-SELECT p.id, p.nome, i.descricao, p.preco
-FROM produto AS p
-JOIN identificacao AS i ON p.id = i.id
-ORDER BY p.preco DESC;
+CREATE TABLE departamento (
+    id_departamento INT PRIMARY KEY,
+    nome_departamento VARCHAR(100),
+    descricao_departamento TEXT
+);
+
+CREATE TABLE gerente (
+    id_gerente INT PRIMARY KEY,
+    nome_gerente VARCHAR(100),
+    salario DOUBLE,
+    id_departamento INT,
+    FOREIGN KEY (id_departamento) REFERENCES departamento(id_departamento)
+);
 
 
-SELECT p.id, p.nome, i.descricao, p.preco
-FROM produto AS p
-JOIN identificacao AS i ON p.id = i.id
-ORDER BY p.nome ASC, p.preco DESC;
+INSERT INTO departamento (id_departamento, nome_departamento, descricao_departamento)
+VALUES (1, 'Vendas', 'Setor responsável por vendas de produtos');
+
+INSERT INTO gerente (id_gerente, nome_gerente, salario, id_departamento)
+VALUES (101, 'João Silva', 5000.00, 1);
 
 
-INSERT INTO produto (nome, preco, validade) VALUES ('azeite', 18.99, '2023-08-12');
-INSERT INTO identificacao (id, nome, descricao) VALUES (10, 'extra virgem', 'prensado a frio');
+SELECT d.nome_departamento, g.nome_gerente, g.salario
+FROM departamento AS d
+JOIN gerente AS g ON d.id_departamento = g.id_departamento
+ORDER BY d.nome_departamento;
 
 
-INSERT INTO produto (nome, preco, validade) VALUES ('vinho', 85.50, '2024-03-20');
-INSERT INTO identificacao (id, nome, descricao) VALUES (11, 'tinto', 'safra 2022');
 
-
-INSERT INTO produto (nome, preco, validade) VALUES ('chocolate', 12.75, '2023-11-05');
-INSERT INTO identificacao (id, nome, descricao) VALUES (12, 'ao leite', '250g');
-
-
+SELECT d.nome_departamento, AVG(g.salario) AS media_salarial
+FROM departamento AS d
+JOIN gerente AS g ON d.id_departamento = g.id_departamento
+GROUP BY d.nome_departamento
+HAVING media_salarial > 4000;
 INSERT INTO produto (nome, preco, validade) VALUES ('whisky', 150.00, '2025-01-15');
 INSERT INTO identificacao (id, nome, descricao) VALUES (13, 'single malt', '12 anos');
